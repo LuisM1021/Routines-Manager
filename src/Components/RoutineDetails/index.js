82a60de9842ad22f4359b29f7ef88196ac4b6b55
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-04-17 19:44:59 -0600

    The information of a routine is displayed in the routine's details panel

--- src/Components/RoutineDetails/index.js
+++ src/Components/RoutineDetails/index.js
@@ -1,24 +1,90 @@
 import './RoutineDetails.css'
 import { GeneralContext } from '../../GeneralContext'
-import { useContext } from 'react'
+import { useContext,useState } from 'react'
+import { ArrowRightCircleIcon,ArrowLeftCircleIcon } from '@heroicons/react/16/solid'
 function RoutineDetails(){
     const {
         setShowRoutineDetails,
-        selectedRoutineDetails: routine
+        selectedRoutineDetails: routine,
+        exercises
     } = useContext(GeneralContext)
     const hideRoutineDetails=(event)=>{
         if(event.target.className ==='background'){
             setShowRoutineDetails(false)
         }
     }
+    console.log(routine)
+    //Storing the index of the current exercise
+    const [currentExercise,setCurrentExercise] = useState(0)
+    const getExercisePath = ()=>{
+        let path
+        const exercise = exercises?.filter(exercise=>exercise.name === routine.exercises[currentExercise])
+        path = exercise[0].imgPath
+        return path
+    }
+    const changeExercise = (direction) => {
+        const lengthOfRoutineExercises = routine.exercises.length
+        if(direction === 'left'){
+            if(currentExercise === 0){
+                setCurrentExercise(lengthOfRoutineExercises-1)
+            }else{
+                setCurrentExercise(currentExercise-1)
+            } 
+        }else{
+            if(currentExercise === lengthOfRoutineExercises-1){
+                setCurrentExercise(0)
+            }else{
+                setCurrentExercise(currentExercise+1)
+            } 
+        }
+    }
+    const showTime= (time)=>{
+        if(time[0]===0 && time[1]===0 && time[2]===0){
+            return(
+                <span></span>
+            )
+        }
+        else if(time[0]>0){
+            return(
+                <span>Time: {time[0]}h {time[1]}m {time[2]}s</span>
+            )
+        }
+        else if(time[0]===0 && time[1]>0){
+            return(
+                <span>Time: {time[1]}m {time[2]}s</span>
+            )
+        }
+        else if(time[0]===0 && time[1]===0){
+            return(
+                <span>Time: {time[2]}s</span>
+            )
+        }
+    }
     return(
         <div className='background' onClick={(event)=>hideRoutineDetails(event)}>
             <div className='routine-details-container'>
                 <h2>{routine.name}</h2>
-                <p>{routine.description}</p>
-                <p>{routine.category}</p>
-                <p>{routine.timer.totalTime}</p>
-                <p>{routine.equipment}</p>
+                <p className='description'>{routine.description}</p>
+                <div className='category-time'>
+                    <p>Category: {routine.category}</p>
+                    <p>{showTime(routine.timer.totalTime)}</p>
+                </div>
+                <p>Require equipment: {routine.equipment}</p>
+                <div className='exercises-panel'>
+                    <div className='exercises-info'>
+                        <h2>{routine.exercises[currentExercise]}</h2>
+                        <p>Series: {routine.series[currentExercise]}</p>
+                        <p>Reps: {routine.reps[currentExercise]}</p>
+                        <p>{showTime(routine.time[currentExercise])}</p>
+                    </div>
+                    <figure className='exercise-image'>
+                        <ArrowLeftCircleIcon className='left-arrow'
+                        onClick={()=>changeExercise('left')}/>
+                        <ArrowRightCircleIcon className='right-arrow'
+                        onClick={()=>changeExercise('right')}/>
+                        <img src={getExercisePath()} alt={routine.exercises[currentExercise]}></img>
+                    </figure>
+                </div>
             </div>
         </div>
     )
