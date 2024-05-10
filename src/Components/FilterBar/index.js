b31674d7f3e33dad0a0b6a0fca8d6af905c0cc70
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-05-10 16:40:25 -0600

    Refactor de routines.css al archivo FilterBar.css

--- src/Components/FilterBar/index.js
+++ src/Components/FilterBar/index.js
@@ -1,6 +1,7 @@
 import { useContext,useState } from "react"
 import { GeneralContext } from "../../GeneralContext"
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline"
+import './FilterBar.css'
 
 function FilterBar(){
     const {
