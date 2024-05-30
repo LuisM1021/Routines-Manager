830a19045d1099a64d2dd4f0f43010558f8e12be
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-05-30 12:18:17 -0600

    Fixed and refactor code in RoutinesPanel files

--- src/Components/RoutinesPanel/index.js
+++ src/Components/RoutinesPanel/index.js
@@ -1,59 +1,81 @@
-import './RoutinesPanel.css'
 import { useContext,useState,useEffect } from 'react'
 import {GeneralContext} from '../../GeneralContext'
+import './RoutinesPanel.css'
 
 function RoutinesPanel(){
     //TODO: getRandom falta de usar
     const {
-        routines,exercises,selectedRoutine,setSelectedRoutine
+        routines,
+        exercises,
+        selectedRoutine,
+        setSelectedRoutine
     } = useContext(GeneralContext)
-    const [selectedImage,setSelectedImage] = useState(null)
 
+    //Saving the routines of the current selected routine
     const routineExercises = selectedRoutine?.exercises?.map(rtExercise=>{
         return(
             exercises.find(exercise=>exercise.name === rtExercise)
         )
     })
+    //Selected image in the controller of images
+    const [selectedImage,setSelectedImage] = useState(null)
+
+    //Initialize the controller to the first image of the routine exercises
     useEffect(()=>{
-        if(routineExercises){
+        if(routineExercises && !selectedImage){
             setSelectedImage(routineExercises[0])
         }
-    },[routineExercises?routineExercises[0]:null])
-    
+    },[routineExercises,selectedImage])
 
     return(
         <div className='panel-container'>
             <div className='routines-list'>
-                <h3>Featured Routines</h3>
-                <li>
+                <h3 className='title-2'>Featured Routines</h3>
+                <li className='featured-routines-container'>
                     {routines?.map((routine,index)=>{
-                        if(index<=4){
-                            return <ul key={routine.name}
-                            onClick={()=>setSelectedRoutine(routine)}>🏋️‍♂️ {routine.name}</ul>
-                        }  else return         
-                    })
-                    }
+                        if(index<=4){ //TODO: SHOW RANDOM ROUTINES NOT THE FIRST FIVE
+                            return <ul
+                                className='featured-routine'
+                                key={routine.name}
+                                onClick={()=>{
+                                setSelectedRoutine(routine)
+                                setSelectedImage(null)
+                                }}>
+                                    🏋️‍♂️ {routine.name}
+                            </ul>
+                        }  else return null       
+                    })}
                 </li>     
             </div>
             <div className="exercises-slider">
                 {selectedImage && (
                     <>
-                        <div className='title-container'><h3>{selectedImage.name}</h3></div>
-                        <img src={selectedImage.imgPath} alt='selectedIMG'></img>
+                        <div className='title-container'>
+                            <h3 className='title-3'>{selectedImage.name}</h3>
+                        </div>
+                        <img
+                            className='exercise-img'
+                            src={selectedImage.imgPath} alt='selectedIMG'
+                        />
                     </>
                 )}
                 <div className='controller'>
                     {routineExercises?.map(exercise=>{
-                        return <div className={
-                            selectedImage && selectedImage.name === exercise.name ? 'slider-button slider-button--active':'slider-button'
-                        } key={exercise.name}
-                        onClick={()=>setSelectedImage(exercise)}></div>
+                        return <div 
+                            className={
+                                selectedImage && selectedImage.name === exercise.name ? 'slider-button slider-button--active':'slider-button'
+                            }
+                            key={exercise.name}
+                            onClick={()=>setSelectedImage(exercise)}>
+                        </div>
                     })}
                 </div>
             </div>
             <div className='exercise-description'>
-                <h3>Description</h3>
-                <p>{selectedRoutine && selectedRoutine.description}</p>
+                <h3 className='title-2'>Description</h3>
+                <p className='description'>
+                    {selectedRoutine && selectedRoutine.description}
+                </p>
             </div>
         </div>
     )
