5707cf6c51c27d03a5e0956c685bb8947c1c96af
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-05-30 12:27:35 -0600

    Fixed and refactor UserRoutines files

--- src/Components/UserRoutines/index.js
+++ src/Components/UserRoutines/index.js
@@ -1,18 +1,21 @@
-import './UserRoutines.css'
 import {PlusIcon} from '@heroicons/react/24/solid'
+import './UserRoutines.css'
+
 function UserRoutines(){
     return(
-        <div className='user-routines-container'>
-            <h2>My routines</h2>
-            <ul>
-                <li>🏋️‍♂️ routine 1</li>
-                <li>🏋️‍♂️ routine 2</li>
-                <li>🏋️‍♂️ routine 3</li>
-                <li>🏋️‍♂️ routine 4</li>
+        <div className='user-routines-panel'>
+            <h2 className='title-2'>My routines</h2>
+            <ul className='user-routines-container'>
+                <li className='user-routine'>🏋️‍♂️ routine 1</li>
+                <li className='user-routine'>🏋️‍♂️ routine 2</li>
+                <li className='user-routine'>🏋️‍♂️ routine 3</li>
+                <li className='user-routine'>🏋️‍♂️ routine 4</li>
             </ul>
             <div className='add-routine-container'>
-                <p>Add routine</p>
-                <figure className='plus-icon-container'><PlusIcon className='plus-icon'/></figure>
+                <p className='add-routine'>Add routine</p>
+                <figure className='plus-icon-container'>
+                    <PlusIcon className='plus-icon'/>
+                </figure>
             </div>
         </div>
         
