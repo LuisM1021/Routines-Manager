acd0239c468f5e662eb61bbd2f862680448d266a
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-05-30 11:57:48 -0600

    Fixed and refactor code in the primary files of the project

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import {useRoutes,BrowserRouter} from 'react-router-dom'
-import { Home } from '../Pages/Home';
-import { Routines } from '../Pages/Routines';
+import { Home } from '../Pages/Home'
+import { Routines } from '../Pages/Routines'
 import { Navbar } from "../Components/Navbar"
-import './App.css';
 
 const AppRoutes = () => {
     const routes = useRoutes([
