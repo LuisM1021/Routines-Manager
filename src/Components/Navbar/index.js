70ab4de199872ad1a83a69ec19e556a7d6d029f7
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-06-06 16:16:23 -0600

    Fixed and refactor Navbar files

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -3,21 +3,31 @@ import './Navbar.css'
 function Navbar(){
     return (
         <aside className="Navbar-container">
-            <img src='https://th.bing.com/th/id/OIG2.eMuuAVBVJ4gZoC6y6kFB?w=270&h=270&c=6&r=0&o=5&dpr=1.3&pid=ImgGn' alt='logo'></img>
+            <img className='logo' src='https://th.bing.com/th/id/OIG2.eMuuAVBVJ4gZoC6y6kFB?w=270&h=270&c=6&r=0&o=5&dpr=1.3&pid=ImgGn' alt='logo'></img>
             <NavLink className='redirect' to='/'>
-                <div><p>Home</p></div> 
+                <div className='page-container'>
+                    <p className='page'>Home</p>
+                </div> 
             </NavLink>
             <NavLink className='redirect' to='/'>
-                <div><p>Timer</p></div> 
+                <div className='page-container'>
+                    <p className='page'>Timer</p>
+                </div> 
             </NavLink>
             <NavLink className='redirect' to='/routines'>
-                <div><p>Routines</p></div> 
+                <div className='page-container'>
+                    <p className='page'>Routines</p>
+                </div> 
             </NavLink>
             <NavLink className='redirect' to='/'>
-                <div><p>Exercises</p></div> 
+                <div className='page-container'>
+                    <p className='page'>Exercises</p>
+                </div> 
             </NavLink>
             <NavLink className='redirect' to='/'>
-                <div><p>Export routine</p></div> 
+                <div className='page-container'>
+                    <p className='page'>Export routine</p>
+                </div> 
             </NavLink>
         </aside>
     )
