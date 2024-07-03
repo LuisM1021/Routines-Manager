1affe5840973cadb25b87ac047d1f71a7083e664
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-07-02 20:53:03 -0600

    development(ExerciseDetail): Added the information required in the card, it has to be organized

--- src/Components/ExerciseDetail/index.js
+++ src/Components/ExerciseDetail/index.js
@@ -13,8 +13,68 @@ function ExerciseDetail(){
             <figure className='exercise-aside__img-container'>
                 <img className='exercise-aside__img' src={exercise.imgPath} alt={exercise.name}/>
             </figure>
-            <h2 className='exercise-aside__description-label'>Description</h2>
+            <h3 className='exercise-aside__description-label'>Description</h3>
             <p className='exercise-aside__description'>{exercise.description}</p>
+            <p className='exercise-aside__category'>
+                <span className='exercise-aside__category-label'>
+                    Category:
+                </span>
+                <span className="exercise-aside__category-name">
+                    {exercise.category}
+                </span>
+            </p>
+            <p className='exercise-aside__muscular-group'>
+                <span className='exercise-aside__muscular-group-label'>
+                    Muscular group:
+                </span>
+                <span className="exercise-aside__muscular-group-name">
+                    {exercise.muscularGroup}
+                </span>
+            </p>
+            <p className='exercise-aside__suggested-reps'>
+                <span className='exercise-aside__suggested-reps-label'>
+                    Suggested Reps:
+                </span>
+                <span className="exercise-aside__suggested-reps-name">
+                    {exercise.suggestedReps}
+                </span>
+            </p>
+            <p className='exercise-aside__suggested-series'>
+                <span className='exercise-aside__suggested-series-label'>
+                    Suggested series:
+                </span>
+                <span className="exercise-aside__suggested-series-name">
+                    {exercise.suggestedSeries}
+                </span>
+            </p>
+            <p className='exercise-aside__suggested-time'>
+                <span className='exercise-aside__suggested-time-label'>
+                    Suggested time:
+                </span>
+                <span className="exercise-aside__suggested-time-name">
+                    {exercise.suggestedTime}
+                </span>
+            </p>
+            {exercise.requireEquipment !== 'No' &&
+             <>   
+                <p className='exercise-aside__equipment'>
+                    <span className='exercise-aside__equipment-label'>
+                        Equipment:
+                    </span>
+                    <span className='exercise-aside__equipment-name'>
+                        {exercise.equipment.name}
+                    </span>
+                </p>
+                <p className='exercise-aside__suggested-weight'>
+                    <span className='exercise-aside__suggested-weight-label'>
+                        Suggested weight:
+                    </span>
+                    <span className='exercise-aside__suggested-weight-name'>
+                        {exercise.equipment.suggestedWeight}
+                    </span>
+                </p>
+            </>
+            }
         </aside>
     )
 }
