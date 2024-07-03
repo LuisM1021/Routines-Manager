ee8fcd76a0333d4653bdd6a321c2e2ca2540ebc9
LuisM1021 <lmurallesp@miumg.edu.gt>
2024-07-03 13:02:56 -0600

    dev(ExerciseDetail): Organizing the data of the card and giving it the first styles

--- src/Components/ExerciseDetail/index.js
+++ src/Components/ExerciseDetail/index.js
@@ -1,5 +1,6 @@
 import { GeneralContext } from "../../GeneralContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/24/outline";
 
 import './ExerciseDetail.css'
 
@@ -7,6 +8,25 @@ function ExerciseDetail(){
     const context = useContext(GeneralContext)
     const exercise = context.displayExerciseDetail
     console.log(exercise)
+    const [showBenefits, setshowBenefits] = useState(false)
+    const [showEquipment, setshowEquipment] = useState(false)
+    const showTime= (exercise)=>{
+        if(exercise.suggestedTime[0]>0 && exercise.suggestedTime[1]>0){
+            return(
+                <span>{exercise.suggestedTime[0]}h {exercise.suggestedTime[1]}m</span>
+            )
+        }
+        else if(exercise.suggestedTime[0]===0 && exercise.suggestedTime[1]>0){
+            return(
+                <span>{exercise.suggestedTime[1]}m</span>
+            )
+        }
+        else if(exercise.suggestedTime[0]===0 && exercise.suggestedTime[1]===0){
+            return(
+                <span>{exercise.suggestedTime[2]}s</span>
+            )
+        }
+    }
     return(
         <aside className='exercise-aside'>
             <h1 className='exercise-aside__title'>{exercise.name}</h1>
@@ -31,6 +51,20 @@ function ExerciseDetail(){
                     {exercise.muscularGroup}
                 </span>
             </p>
+            <div className='exercise-aside__benefits'>
+                <button className={`exercise-aside__open-benefits ${showBenefits && 'exercise-aside__open-benefits--active'}`}
+                onClick={()=>showBenefits ? setshowBenefits(false) : setshowBenefits(true) }>
+                    <span className='exercise-aside__benefits'>Benefits</span>
+                    <ChevronDownIcon className='exercise-aside__benefits-arrow-down'/>
+                </button>
+                {showBenefits && 
+                    <ul className='exercise-aside__benefits-list'>
+                        {exercise.benefits.benefit.map((item,index) => 
+                            <li className='exercise-aside__benefit-item' key={index}>{item}</li>
+                        )}
+                    </ul>
+                }
+            </div>
             <p className='exercise-aside__suggested-reps'>
                 <span className='exercise-aside__suggested-reps-label'>
                     Suggested Reps:
@@ -47,17 +81,30 @@ function ExerciseDetail(){
                     {exercise.suggestedSeries}
                 </span>
             </p>
-            <p className='exercise-aside__suggested-time'>
-                <span className='exercise-aside__suggested-time-label'>
-                    Suggested time:
-                </span>
-                <span className="exercise-aside__suggested-time-name">
-                    {exercise.suggestedTime}
-                </span>
-            </p>
+            {(exercise.suggestedTime[0] !== 0 || exercise.suggestedTime[1] !== 0 || exercise.suggestedTime[2] !== 0) && 
+                <p className='exercise-aside__suggested-time'>
+                    <span className='exercise-aside__suggested-time-label'>
+                        Suggested time:
+                    </span>
+                    {showTime(exercise)}
+                </p>
+            }
             {exercise.requireEquipment !== 'No' &&
              <>   
-                <p className='exercise-aside__equipment'>
+                <div className='exercise-aside__equipment'>
+                    <button className={`exercise-aside__open-equipment ${showEquipment && 'exercise-aside__open-equipment--active'}`}
+                    onClick={()=>showEquipment ? setshowEquipment(false) : setshowEquipment(true) }>
+                        <span className='exercise-aside__equipment-label'>Equipment</span>
+                        <ChevronDownIcon className='exercise-aside__equipment-arrow-down'/>
+                    </button>
+                    {showEquipment && 
+                        <p>
+                            <span>{exercise.equipment.name}</span>
+                            <span>{exercise.equipment.suggestedWeight}</span>
+                        </p>
+                    }
+                </div>
+                {/* <p className='exercise-aside__equipment'>
                     <span className='exercise-aside__equipment-label'>
                         Equipment:
                     </span>
@@ -72,7 +119,7 @@ function ExerciseDetail(){
                     <span className='exercise-aside__suggested-weight-name'>
                         {exercise.equipment.suggestedWeight}
                     </span>
-                </p>
+                </p> */}
             </>
             }
         </aside>
